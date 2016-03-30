118dcc990e1e07fa0b44e51c8a87412c9447bce7
Brett Patterson <brett.patterson@rice.edu>
2016-03-29 23:06:42 -0500

    Complete tests for full coverage

--- tests/test_user.py
+++ tests/test_user.py
@@ -9,6 +9,8 @@ class TestGraph(TestCase):
         node = UserNode(tag)
         self.assertEqual(tag, node.tag)
 
+        self.assertEqual('User<5>', str(node))
+
     def test_neighbors(self):
         node = UserNode(1)
         node_2 = UserNode(2)
@@ -35,7 +37,7 @@ class TestGraph(TestCase):
 
     def test_graph(self):
         adj_list = {0: [1, 2], 1: [0, 2], 2: [0, 1], 3: []}
-        graph = UserGraph(adj_list)
+        graph = UserGraph(adj_list, {3: {'foo': 'bar'}})
 
         self.assertEquals(
             set([0, 1, 2, 3]),
@@ -43,3 +45,4 @@ class TestGraph(TestCase):
         )
 
         self.assertEquals(2, graph.get_user(2).tag)
+        self.assertEquals('bar', graph.get_user(3).metadata['foo'])
